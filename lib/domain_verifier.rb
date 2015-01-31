1e3623ba6f56f3d95b5cb13c08eac78001318c8f
Harald Wartig <hwartig@gmail.com>
2015-01-31 18:35:51 +0100

    Add Domain Class

--- lib/domain_verifier.rb
+++ lib/domain_verifier.rb
@@ -1,5 +1,37 @@
-require "domain_verifier/version"
+require 'domain_verifier/version'
+require 'whois'
 
 module DomainVerifier
-  # Your code goes here...
+  def self.verify(name)
+    Domain.new(name).verify
+  end
+
+  class Domain
+    attr_reader :name
+
+    def initialize(name)
+      @name = name.downcase
+    end
+
+    def whois
+      @whois ||= Whois.whois(name)
+    end
+
+    def http_status
+      @status ||= begin
+        http = Net::HTTP.start(name)
+        http.head('/').code
+      end
+    end
+
+    def to_s
+      name
+    end
+
+    def verify
+      whois
+      http_status
+      self
+    end
+  end
 end
--- spec/domain_verifier_spec.rb
+++ spec/domain_verifier_spec.rb
@@ -1,11 +1,73 @@
 require 'spec_helper'
 
 describe DomainVerifier do
+  let(:name) { 'hWartig.com' }
+
   it 'has a version number' do
     expect(DomainVerifier::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '.verify' do
+    subject { described_class.verify(name) }
+    let(:result) { double('domain') }
+
+    before do
+      expect_any_instance_of(DomainVerifier::Domain).to receive(:verify)
+        .and_return(result)
+    end
+
+    it { should eq(result) }
+  end
+
+  describe DomainVerifier::Domain do
+    subject(:domain) { described_class.new(name) }
+
+    it { should respond_to(:whois) }
+
+    describe '#whois' do
+      it 'delegates work to whois' do
+        expect(Whois).to receive(:whois).with(name.downcase)
+          .and_return('whois result')
+        expect(subject.whois).to eq('whois result')
+      end
+    end
+
+    it { should respond_to(:to_s) }
+
+    describe '#to_s' do
+      subject { super().to_s }
+
+      it { should eq(name.downcase) }
+    end
+
+    it { should respond_to(:http_status) }
+
+    describe '#http_status' do
+      subject { super().http_status }
+
+      context 'when site is alive' do
+        let(:http) { double('http') }
+
+        before do
+          expect(Net::HTTP).to receive(:start).with(name.downcase)
+            .and_return(http)
+          expect(http).to receive(:head).with('/')
+            .and_return(Struct.new(:code).new(200))
+        end
+
+        it { should eq(200) }
+      end
+    end
+
+    describe '#verify' do
+      subject { super().verify }
+
+      before do
+        expect(domain).to receive(:whois)
+        expect(domain).to receive(:http_status)
+      end
+
+      it { should eq(domain) }
+    end
   end
 end
